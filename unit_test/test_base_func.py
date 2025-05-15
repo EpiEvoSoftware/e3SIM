b8b8a7ed5950fd82f9e8fa48121a54b1d8be5392
perryXuu <natsuyuu.x@gmail.com>
2025-05-15 15:59:13 -0400

    Add test cases for the base functions

--- unit_test/test_base_func.py
+++ unit_test/test_base_func.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pytest
 
-curr_dir = os.path.dirname(__file__)curr_dir = os.path.dirname(__file__)
+curr_dir = os.path.dirname(__file__)
 e3SIM_dir = os.path.join(curr_dir, '../e3SIM_codes')
 if e3SIM_dir not in sys.path:
 	sys.path.insert(0, e3SIM_dir)
